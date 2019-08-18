a34119d0769b46a5b3d5bb833af0582ca59f374d
rholderfield <raholderfield@gmail.com>
2019-08-18 16:59:36 -0700

    Adding modal for new book

--- main.js
+++ main.js
@@ -236,4 +236,22 @@ function deleteUI() {
     });
 }
 
+(function() {
+  "use strict";
+  var dialog = document.querySelector("#modal-example");
+  var closeButton = dialog.querySelector("button");
+  var showButton = document.querySelector("#show-modal-example");
+  if (!dialog.showModal) {
+    dialogPolyfill.registerDialog(dialog);
+  }
+  var closeClickHandler = function(event) {
+    dialog.close();
+  };
+  var showClickHandler = function(event) {
+    dialog.showModal();
+  };
+  showButton.addEventListener("click", showClickHandler);
+  closeButton.addEventListener("click", closeClickHandler);
+})();
+
 buildUI();
